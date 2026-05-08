53241312c6811a18e31a3af8e06eab6cb02e479f
Yvens Porto <yvensianpporto@gmail.com>
2026-05-08 11:25:49 -0300

    add pizzeria

--- CSES/RangeQueries/ PizzeriaQueries.cpp
+++ CSES/RangeQueries/ PizzeriaQueries.cpp
@@ -1,82 +1,74 @@
 #include <bits/stdc++.h>
-using namespace std;
 #define pb push_back
 #define all(x) x.begin(), x.end()
-#define sz(x) (int) x.size()
+#define sz(x) x.size()
 #define pc __builtin_popcount
-
-typedef long long ll;
-
-const int MAX = 2e5+20;
-const ll LINF = 4e18;
-
+ 
+using namespace std;
+ 
+const int MAX = 2e5;
+const int INF = 2e9;
+int v[MAX], n, q;
+ 
+typedef long long int ll;
+ 
 namespace seg {
-	ll seg[4*MAX], lazy[4*MAX];
-	ll n, *v;
-
-	ll build(int p=1, int l=0, int r=n-1) {
-		lazy[p] = 0;
-		if (l == r) return seg[p] = v[l];
+	pair<ll, ll> seg[4*MAX];
+ 
+	pair<ll, ll> build(int p=1, int l=0, int r=n-1) {
+		if (l == r) return seg[p] = {v[l] - l, v[l] + l};
 		int m = (l+r)/2;
-		return seg[p] = max(build(2*p, l, m), build(2*p+1, m+1, r));
+        auto esq = build(2*p, l, m), dir = build(2*p+1, m+1, r);
+		return seg[p] = {min(esq.first, dir.first), min(esq.second, dir.second)};
 	}
-	void build(int n2, ll* v2) {
-		n = n2, v = v2;
-		build();
-	}
-	void prop(int p, int l, int r) {
-		seg[p] += lazy[p];
-		if (l != r) lazy[2*p] += lazy[p], lazy[2*p+1] += lazy[p];
-		lazy[p] = 0;
-	}
-	ll query(int a, int b, int p=1, int l=0, int r=n-1) {
-		prop(p, l, r);
+	pair<ll, ll> query(int a, int b, int p=1, int l=0, int r=n-1) {
 		if (a <= l and r <= b) return seg[p];
-		if (b < l or r < a) return -LINF;
+		if (b < l or r < a) return {INF, INF};
 		int m = (l+r)/2;
-		return max(query(a, b, 2*p, l, m), query(a, b, 2*p+1, m+1, r));
+        auto esq = query(a, b, 2*p, l, m), dir = query(a, b, 2*p+1, m+1, r);
+		return {min(esq.first, dir.first), min(esq.second, dir.second)};
 	}
-	ll update(int a, int b, int x, int p=1, int l=0, int r=n-1) {
-		prop(p, l, r);
-		if (a <= l and r <= b) {
-			lazy[p] += x;
-			prop(p, l, r);
-			return seg[p];
+	void update(int a, int x, int p=1, int l=0, int r=n-1) {
+		if (l == r) {
+			seg[p] = {x - l, x + l};
 		}
-		if (b < l or r < a) return seg[p];
-		int m = (l+r)/2;
-		return seg[p] = max(update(a, b, x, 2*p, l, m),
-			update(a, b, x, 2*p+1, m+1, r));
+        else {
+            int m = (l+r)/2;
+            if (a <= m)
+                update(a, x, p*2, l, m);
+            else
+                update(a, x, p*2+1, m+1, r);
+            auto esq = seg[p*2], dir = seg[p*2+1];
+		    seg[p] = {min(esq.first, dir.first), min(esq.second, dir.second)};
+        }
 	}
 };
  
-void solve() {
-    int n, q; cin >> n >> q;
-    ll x[n + 1], pref[n + 1]; x[0] = 0; pref[0] = 0;
-    for(int i = 1; i <= n; i++) {
-        cin >> x[i]; pref[i] = pref[i - 1] + x[i];
+void solve(){
+    cin >> n >> q;
+ 
+    for(int i = 0; i < n; i++) {
+        cin >> v[i];
     }
-
-    seg::build(n + 1, pref);
-    
+ 
+    seg::build();
+ 
     for(int i = 0; i < q; i++) {
-        int c; cin >> c;
-        int a, b; cin >> a >> b;
-
-        if(c == 1) {
-            seg::update(a, n, b - x[a]);
-            x[a] = b;
+        int qi; cin >> qi;
+        if(qi == 1) {
+            int k, x; cin >> k >> x; k--;
+            seg::update(k, x);
         }
         else {
-            ll ans = seg::query(a, b) - seg::query(a - 1, a - 1);
-            cout << max(ans, 0LL) << '\n';
+            int k; cin >> k; k--;
+            int esq = seg::query(0, k).first + k;
+            int dir = seg::query(k, n - 1).second - k;
+            cout << min(esq, dir) << '\n';
         }
-
     }
-
 }
-
-signed main() {
+ 
+int main() {
     ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
     solve();
     return 0;
